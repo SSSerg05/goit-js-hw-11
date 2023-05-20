c793a07cbeffa10374ec2e8274cc7ed19bca3720
SSSerg05 <ssserg05@gmail.com>
2023-05-20 23:06:28 +0300

    add class Gallery
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

---
+++ src/js/Gallery.js
@@ -0,0 +1,48 @@
+const axios = require("axios/dist/axios.min.js"); // node
+
+const URL = 'https://pixabay.com/api/';
+const API_KEY = '36214966-0d101d8d6f502ad642532aad3';
+const PER_PAGE = 40;
+
+export default class Gallery {
+  constructor () {
+    this.page = 1;
+    this.searchQuery = '';
+  }
+
+  getPictures() {
+    const params = {
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: PER_PAGE,
+    }
+
+    return axios
+      .get(
+        `${URL}`, params 
+      )
+      .then(function (response) {
+        this.incrementPage();
+
+
+      // if not find search 
+        if (response.data.hits.length === 0)  {
+          Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+          return
+        }
+      })
+      .catch(
+        function (error) {
+          Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+          console.log(error);
+      })
+  }
+
+  incrementPage() {
+    this.page++;
+  }
+}
